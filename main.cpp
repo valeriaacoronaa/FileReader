d6532d678787c89cc1b22aae4f1916741e4bb99a
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-10-03 12:44:54 -0700

    reads files

--- main.cpp
+++ main.cpp
@@ -6,57 +6,17 @@ using namespace std;
 
 int main()
 {
-        string fileName; //to get the name of the file to open
-        string line; //to store a single line of a text file
-        fstream fileStream; //to open a file for reading
-        int lines=0;
-        int characters=0;
-        char answer= ' ';
-
-        cout<<"What file do you want to open? ";
-        getline(cin, fileName);
-
-        //STEP 1: open the fileStream for input, using the fileName specified
-        fileStream.open(fileName.c_str(), ios::in);
-        if( /*STEP 2: check to see if the fileStream successfully opened*/fileStream.is_open())
-        {
-                cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-
-                //STEP 3: repeat the following until the end-of-file (eof) has been reached...
-                // 3A: read a line from fileStream into the variable line
-                // 3B: display the line, followed by an endline
-                while(!fileStream.eof())
-                {
-                        getline(fileStream, line);
-                        if(!line.empty())
-                        {
-                                cout << line << endl;
-                                lines+=1; 
-                                characters= line.length()+characters;
-                        }
-                }   
-
-                //STEP 4: close the fileStream
-
-        }
-        else
-        {
-                cout<<fileName<<" could not be opened.\n";
-        }
-        cout << "METADATA\n" << "File: " << fileName << "\n" <<"Lines: " << lines << "\n" << "Characters: " << characters << "\n";
-        cout << "Analyze another file (y/n)? ";
-        cin  >> answer;
-        while(answer!='n' ||answer!='N')
+        char answer= 'y';
+        while(answer=='y' ||answer=='Y')
         {
                 string fileName;
                 string line;
                 fstream fileStream;
                 int lines=0;
                 int characters=0;
-                char answer=' ';
-
                 cout<<"What file do you want to open? ";
-                cin>> fileName;
+                getline(cin, fileName);
+
                 //STEP 1: open the fileStream for input, using the fileName specified
                 fileStream.open(fileName.c_str(), ios::in);
                 if( /*STEP 2: check to see if the fileStream successfully opened*/fileStream.is_open())
@@ -72,10 +32,10 @@ int main()
                                 if(!line.empty())
                                 {
                                         cout << line << endl;
-                                        lines+=1;
+                                        lines+=1; 
                                         characters= line.length()+characters;
                                 }
-                        }
+                        }   
 
                         //STEP 4: close the fileStream
 
@@ -87,11 +47,10 @@ int main()
                 cout << "METADATA\n" << "File: " << fileName << "\n" <<"Lines: " << lines << "\n" << "Characters: " << characters << "\n";
                 cout << "Analyze another file (y/n)? ";
                 cin  >> answer;
-        if(answer=='n'||answer=='N')
-        {
-                return 0;
-        }
-        }
+                cin.ignore();
         fileStream.close();
+
+        }
+
         return 0;
 }
