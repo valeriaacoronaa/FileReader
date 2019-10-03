782ca7849102ebdfde08f575057676a0476adf8e
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-10-02 21:55:30 -0700

    analyzes multiple files

--- main.cpp
+++ main.cpp
@@ -9,17 +9,18 @@ int main()
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
+  fstream filestream;
   int numberOflines=0;
   int numberOfcharacters=0;
   char answer=' ';
-  string fileName2;
+  int lines=0;
+  int characters=0;
 
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
   
-  //STEP 1: open the fileStream for input, using the fileName specified
   fileStream.open(fileName.c_str(), ios::in);
-  if( /*STEP 2: check to see if the fileStream successfully opened*/fileStream.is_open())
+  if(fileStream.is_open())
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
     
@@ -46,39 +47,40 @@ int main()
    cout << "Analyze another file (y/n)?";
    cin  >> answer;
    
-  while(answer == 'y' || answer == 'Y')
+  do
   {
+    string fileName;
     cout<<" What file do you want to open? ";
-    cin >> fileName2;
-  fileStream.open(fileName2.c_str(), ios::in);
-  if( /*STEP 2: check to see if the fileStream successfully opened*/fileStream.is_open())
-  {
-    cout<<fileName2<<" opened.\nFILE CONTENTS:\n";
+    cin >> fileName;
+  filestream.open(fileName.c_str(), ios::in);
+    if( filestream.is_open())
+     {
+         cout<<fileName<<" opened.\nFILE CONTENTS:\n";
 
 
-    while(!fileStream.eof())
-    {
-        getline(fileStream, line);
-        if(!line.empty())
-        {
-        cout << line << endl;
-        numberOflines+=1;
-        numberOfcharacters= line.length()+numberOfcharacters;
-        }
-    }
-  }
-  else
-  {
-    cout<<fileName2<<" could not be opened.\n";
-  }
+          while(!filestream.eof())
+          {
+             getline(filestream, line);
+             if(!line.empty())
+             {
+                 cout << line << endl;
+                 lines+=1;
+                 characters= line.length()+characters;
+              }
+            }
+       } else
+         {
+             cout<<fileName<<" could not be opened.\n";
+         }
 
-   cout << "METADATA\n" << "File: " << fileName2 << "\n" <<"Lines: " << numberOflines << "\n" << "Characters: " << numberOfcharacters << "\n";
+   cout << "METADATA\n" << "File: " << fileName << "\n" <<"Lines: " << lines << "\n" << "Characters: " << characters << "\n";
 
-   cout << "Analyze another file (y/n)?";
+   cout << "Analyze another file (y/n)?\n";
    cin  >> answer;
-   }
+   }while(answer == 'y' || answer == 'Y');   
 
    fileStream.close();
+   filestream.close();
 
   return 0;
 }
